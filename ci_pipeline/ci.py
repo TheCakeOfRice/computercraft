8c74d2a3fd48eba4807a698d0ee5e5c86b4f43ee
thecakeofrice <ericjrice95@gmail.com>
2024-04-13 10:09:52 -0700

    testing ci pipeline

---
+++ ci_pipeline/ci.py
@@ -0,0 +1,14 @@
+import requests
+import os
+
+readme = requests.get(f'https://api.github.com/repos/TheCakeOfRice/computercraft/contents/ci_pipeline/pastebins.txt?ref="mulungus-server"')
+print(readme.text)
+# Loops through all .lua files
+for cpu_folder in os.listdir("./computers"):
+    for filename in os.listdir(f"./computers/{cpu_folder}"):
+        # Check if the current file is a regular file (not a directory)
+        if filename.endswith(".lua"):
+            # Do something with the file
+            print(f"{cpu_folder}: {filename}")
+# POST to pastebin
+# publish .txt of map of file to pastebin hashes
