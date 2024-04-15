c847000635fc05cc2e8de501ccba22dfb9a6e694
thecakeofrice <ericjrice95@gmail.com>
2024-04-15 16:45:48 -0700

    add deluxe cheeseburger recipe

--- computers/MacGyver/recipes.lua
+++ computers/MacGyver/recipes.lua
@@ -9,3 +9,14 @@ CHEST = {
     "minecraft:spruce_planks",
     "minecraft:spruce_planks"
 }
+DELUXE_CHEESEBURGER = {
+    "minecraft:bread",
+    "farmersdelight:beef_patty",
+    "minecraft:milk_bucket",
+    "farmersdelight:beef_patty",
+    "minecraft:milk_bucket",
+    "farmersdelight:cooked_bacon",
+    "farmersdelight:cabbage_leaf",
+    "farmersdelight:tomato",
+    "farmersdelight:onion"
+}
