b0b7e458de037abc35b0b63d692fa2390834b4a0
thecakeofrice <ericjrice95@gmail.com>
2024-04-16 16:29:51 -0700

    implement home screen

--- computers/iPad/gui.lua
+++ computers/iPad/gui.lua
@@ -1,6 +1,30 @@
 os.loadAPI("funcs.lua")
 
-function draw(inv, y)
+function drawHome()
+    term.clear()
+
+    local no_text_line = "                          "
+    local white_line = "00000000000000000000000000"
+    local red_line = "eeeeeeeeeeeeeeeeeeeeeeeeee"
+    local blue_line = "bbbbbbbbbbbbbbbbbbbbbbbbbb"
+
+    for y=1, 20 do
+        -- inventory button on top half of screen, white text, red background
+        if y == 5 then
+            term.blit("        Inventory         ", white_line, red_line)
+        elseif y <= 10 then
+            term.blit(no_text_line, white_line, red_line)
+        
+        -- git pull button on bottom half of screen, white text, blue background
+        elseif y == 15 then
+            term.blit("         Git Pull         ", white_line, blue_line)
+        else
+            term.blit(no_text_line, white_line, blue_line)
+        end
+    end
+end
+
+function drawInv(inv, y)
     term.clear()
     for i, item in ipairs(inv) do
         -- only write up to 19 lines
@@ -21,8 +45,9 @@ function draw(inv, y)
 
     -- write buttons
     term.setCursorPos(1, 20)
-    term.blit(" Search ", "00000000", "bbbbbbbb")
-    term.blit(" Deposit Last Row ", "000000000000000000", "eeeeeeeeeeeeeeeeee")
+    term.blit(" Home  ", "0000000", "ddddddd")
+    term.blit(" Search  ", "000000000", "bbbbbbbbb")
+    term.blit(" Deposit  ", "0000000000", "eeeeeeeeee")
 end
 
 function drawSuccess()
--- computers/iPad/startup.lua
+++ computers/iPad/startup.lua
@@ -2,64 +2,95 @@ os.loadAPI("funcs.lua")
 os.loadAPI("gui.lua")
 
 rednet.open("back")
+
+-- init state variable
+local state = "home"
+
+-- start gui on home screen
+gui.drawHome()
+
+-- init variables for "inv" state
 local inv = funcs.inventory()
 local scroll_y = 1
-gui.draw(inv, scroll_y)
 
 while true do
-    local event, dir, x, y = os.pullEvent()
-    if event == "mouse_click" then
-        -- a button was clicked
-        if y == 20 then
-            -- search was clicked
-            if x <= 8 then
-                gui.drawSearchPrompt()
-                local searchTerm = read()
+    while state == "home" do
+        local event, dir, x, y = os.pullEvent()
+        if event == "mouse_click" then
+            -- inventory was clicked
+            if y <= 10 then
+                state = "inv"
                 inv = funcs.inventory()
-                inv = funcs.search(inv, searchTerm)
                 scroll_y = 1
+                gui.drawInv(inv, scroll_y)
 
-            -- deposit last row was clicked
-            elseif x > 8 then
-                if funcs.depositLastRow() then
-                    gui.drawSuccess()
-                else
-                    gui.drawFailed()
-                end
-
-                -- update inventory
-                inv = funcs.inventory()
-            end
-        
-        -- an item was clicked
-        else
-            if inv[scroll_y + y - 1] then
-                -- get user input for quantity
-                gui.drawQuantityPrompt()
-                local count = tonumber(read())
-
-                -- request items
-                if funcs.get(inv[scroll_y + y - 1].name, count) then
-                    gui.drawSuccess()
-                else
-                    gui.drawFailed()
-                end
-
-                -- update inventory
-                inv = funcs.inventory()
+            -- git pull was clicked
+            else
+                os.run("gitPull.lua")
             end
         end
+    end
 
-        -- re-draw
-        gui.draw(inv, scroll_y)
+    while state == "inv" do
+        local event, dir, x, y = os.pullEvent()
+        if event == "mouse_click" then
+            -- a button was clicked
+            if y == 20 then
+                -- home was clicked
+                if x <= 7 then
+                    state = "home"
+                    gui.drawHome()
 
-    elseif event == "mouse_scroll" then
-        -- update scroll variable
-        scroll_y = scroll_y + dir
-        scroll_y = math.min(scroll_y, #inv - 18)
-        scroll_y = math.max(scroll_y, 1)
+                -- search was clicked
+                elseif x >= 8 and x < 18 then
+                    gui.drawSearchPrompt()
+                    local searchTerm = read()
+                    inv = funcs.inventory()
+                    inv = funcs.search(inv, searchTerm)
+                    scroll_y = 1
 
-        -- re-draw
-        gui.draw(inv, scroll_y)
+                -- deposit was clicked
+                else
+                    if funcs.depositLastRow() then
+                        gui.drawSuccess()
+                    else
+                        gui.drawFailed()
+                    end
+
+                    -- update inventory
+                    inv = funcs.inventory()
+                end
+            
+            -- an item was clicked
+            else
+                if inv[scroll_y + y - 1] then
+                    -- get user input for quantity
+                    gui.drawQuantityPrompt()
+                    local count = tonumber(read())
+
+                    -- request items
+                    if funcs.get(inv[scroll_y + y - 1].name, count) then
+                        gui.drawSuccess()
+                    else
+                        gui.drawFailed()
+                    end
+
+                    -- update inventory
+                    inv = funcs.inventory()
+                end
+            end
+
+            -- re-draw
+            gui.drawInv(inv, scroll_y)
+
+        elseif event == "mouse_scroll" then
+            -- update scroll variable
+            scroll_y = scroll_y + dir
+            scroll_y = math.min(scroll_y, #inv - 18)
+            scroll_y = math.max(scroll_y, 1)
+
+            -- re-draw
+            gui.drawInv(inv, scroll_y)
+        end
     end
 end
