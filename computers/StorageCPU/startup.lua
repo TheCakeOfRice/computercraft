66423dc04e3c098ab84c7bde43aa9b2c0cb4f864
thecakeofrice <ericjrice95@gmail.com>
2024-04-21 11:47:03 -0700

    update print statements

--- computers/StorageCPU/startup.lua
+++ computers/StorageCPU/startup.lua
@@ -2,26 +2,27 @@ local funcs = require("funcs")
 local vars = require("vars")
 local cd = require("_cd_pipeline._cd")
 
+print("Processing queue from APIServer...")
 rednet.open(vars.WIRED_MODEM_SIDE)
 while true do
     rednet.send(vars.API_SERVER, { method="pop" })
     local _, message = rednet.receive(nil, 3)
     if message then
         if message.method == "inventory" then
-            print("Received 'inventory'.")
+            print("Executing 'inventory'.")
             local inv = funcs.getInventory()
             rednet.send(message.cpu, inv)
         elseif message.method == "get" then
-            print("Received 'get "..message.item.." "..tostring(message.count).."'.")
+            print("Executing 'get "..message.item.." "..tostring(message.count).."'.")
             rednet.send(message.cpu, funcs.sendToPlayer(message.item, message.count))
         elseif message.method == "export" then
-            print("Received 'export "..message.item.." "..tostring(message.count).."'.")
+            print("Executing 'export "..message.item.." "..tostring(message.count).."'.")
             rednet.send(message.cpu, funcs.export(message.target, message.item, message.count, message.toSlot))
         elseif message.method == "depositLastRow" then
-            print("Received 'depositLastRow'.")
+            print("Executing 'depositLastRow'.")
             rednet.send(message.cpu, funcs.depositLastRow())
         elseif message.method == "deposit" then
-            print("Received 'deposit'.")
+            print("Executing 'deposit'.")
             rednet.send(message.cpu, funcs.deposit())
         elseif message.method == "gitPull" then
             print("Pulling from GitHub...")
@@ -31,7 +32,7 @@ while true do
     end
 
     if redstone.getAnalogInput("top") > 0 then
-        print("Depositing items...")
+        print("Redstone triggered 'deposit'.")
         funcs.deposit()
     end
 end
