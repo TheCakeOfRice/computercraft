80f000ed6db89b544b858240a27063dcb649d86f
thecakeofrice <ericjrice95@gmail.com>
2024-04-21 12:22:39 -0700

    insert GitHub PAT from env.lua

--- computers/iPad/gitPull.lua
+++ computers/iPad/gitPull.lua
@@ -1,13 +1,21 @@
 local vars = require("vars")
 local cd = require("_cd_pipeline._cd")
+local env = require("env")
 
 term.clear()
 
 -- get file map
 local branch = arg[1] or "master"
-local mapURL = "https://api.github.com/repos/TheCakeOfRice/computercraft/contents/ci_pipeline/file_map.json?ref="..tostring(branch)
+local mapURL = "https://"..env.GITHUB_API_KEY.."@api.github.com/repos/TheCakeOfRice/computercraft/contents/ci_pipeline/file_map.json?ref="..tostring(branch)
 local _, fileMap = cd.getGitHubFile(mapURL)
 fileMap = textutils.unserializeJSON(fileMap)
+
+-- add PAT in URLs
+local prefix = "http://"
+for cpuName, url in pairs(fileMap) do
+    local suffix = string.sub(url, #prefix + 1, #url)
+    fileMap[cpuName] = prefix..env.GITHUB_API_KEY.."@"..suffix
+end
 
 -- update files on iPad
 local pulled = cd.updateFiles("iPad", fileMap)
