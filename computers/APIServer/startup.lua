d5fb805d640407945a35a0d690d4db1cc737e671
thecakeofrice <ericjrice95@gmail.com>
2024-05-06 20:37:04 -0700

    add instant reply when pop is nil

--- computers/APIServer/startup.lua
+++ computers/APIServer/startup.lua
@@ -9,31 +9,35 @@ rednet.open(vars.ENDER_MODEM_SIDE)
 print("Listening for rednet messages...")
 while true do
     local cpu, message = rednet.receive(nil, 60)
-    if message.method == "pop" then
-        local next = funcs.pop(queue)
-        if next then
-            rednet.send(cpu, next)
-            print(" -- executing "..next.method)
+    if message then
+        if message.method == "pop" then
+            local next = funcs.pop(queue)
+            if next then
+                rednet.send(cpu, next)
+                print(" -- executing "..next.method)
+            else
+                rednet.send(cpu, false)
+            end
+        elseif message.method == "status" then
+            rednet.send(cpu, queue)
+        elseif message.method == "gitPull" then
+            print("Pulling from GitHub...")
+            local pulled = cd.updateFiles("APIServer", message.fileMap)
+
+            -- forward pull request
+            local cpus = funcs.concat({ peripheral.find("turtle") }, { peripheral.find("computer") })
+            for _, cpu in ipairs(cpus) do
+                rednet.send(cpu.getID(), message)
+            end
+
+            -- give feedback to initator
+            rednet.send(cpu, true)
+
+            if pulled then os.reboot() end
+        else
+            funcs.enqueue(queue, cpu, message)
+            print(" -- queueing "..message.method)
         end
-    elseif message.method == "status" then
-        rednet.send(cpu, queue)
-    elseif message.method == "gitPull" then
-        print("Pulling from GitHub...")
-        local pulled = cd.updateFiles("APIServer", message.fileMap)
-
-        -- forward pull request
-        local cpus = funcs.concat({ peripheral.find("turtle") }, { peripheral.find("computer") })
-        for _, cpu in ipairs(cpus) do
-            rednet.send(cpu.getID(), message)
-        end
-
-        -- give feedback to initator
-        rednet.send(cpu, true)
-
-        if pulled then os.reboot() end
-    else
-        funcs.enqueue(queue, cpu, message)
-        print(" -- queueing "..message.method)
     end
     monitor.drawQueue(queue)
 end
